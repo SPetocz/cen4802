77b0863a3508a5ab209200d6f9528dbaf46966c7
Steven Petocz <stevenpetocz@gmail.com>
2026-06-10 19:50:50 -0400

    Commit and push initial version of project to main branch.

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args){
+        int n = 10;
+        int term = fibonacci(n);
+        System.out.println("\nThe " + n + "th term of the Fibonacci sequence is " + term + ".");
+    }
+
+    public static int fibonacci(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        return (fibonacci(n-1) + fibonacci(n-2));
+    }
+
+}
